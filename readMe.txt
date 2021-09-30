32161108aa4cf69142af123ec721ec8a35131676
Robby <blumrj@gmail.com>
2021-09-30 11:38:02 -0400

    mvp

--- readMe.txt
+++ readMe.txt
@@ -1,1 +1,4 @@
 Tool to generate xml button map from a directory of sound files for Voicemeeter Macro Buttons
+
+Ref
+https://docs.python.org/3/library/xml.etree.elementtree.html
--- soundBoardGen.py
+++ soundBoardGen.py
@@ -1,28 +1,42 @@
+import glob
 import os
 import xml.etree.ElementTree as ET
+import sys
 
-soundList = os.listdir("C:\Users\rblum\Google Drive\Game Files\Sounds & Songs\")
+path = r"C:\Users\rblum\Google Drive\Game Files\soundBoard\Apex\*.mp3"
+soundPath = glob.glob( path )
+
+i = 0
+while i < len(soundPath):
+#   print(soundPath[i])
+   print(os.path.basename(soundPath[i]))
+   i = i + 1
 
 
 VBAudioVoicemeeterMacroButtonMap = ET.Element('VBAudioVoicemeeterMacroButtonMap')
-   MacroButtonConfiguration = ET.SubElement(VBAudioVoicemeeterMacroButtonMap, 'MacroButtonConfiguration', x0='681' y0='304' dx='873' dy='576')
+MacroButtonConfiguration = ET.SubElement(VBAudioVoicemeeterMacroButtonMap, 'MacroButtonConfiguration', x0='681', y0='304', dx='873', dy='576')
 
-   for i in range (1,80)
-      MacroButton = ET.SubElement(MacroButtonConfiguration, 'MacroButton', index='i' type='1' color='3' key='85' ctrl='0' shift='0' alt='0' anyway='0' exclusive='0' trigger='0' xinput='0' )
-         MB_MIDI = ET.SubElement( MacroButton, 'MB_MIDI', b1='00' b2='00' b3='00' b4='00' b5='00' b6='00')
-         MB_TRIGGER = ET.SubElement( MacroButton, 'MB_TRIGGER', tchannel='0' tin='0.0' tout='0.0' tmsHold='100' tafterMute='0')
-         MB_XINPUT = ET.SubElement( MacroButton, 'MB_XINPUT', nctrl='0' nbutton='0')
-         MB_Name = ET.SubElement( MacroButton, 'MB_Name')
-         MB_Name.text = soundList(i) #first N letters of File Name
-         MB_Subname = ET.SubElement( MacroButton, 'MB_Subname')
-         MB_Subname.text = soundList(i) ##remaining N letters of File Name
-         MB_InitRequest = ET.SubElement( MacroButton, 'MB_InitRequest')
-         MB_InitRequest.text = 'Recorder.mode.PlayOnLoad=1'
-         MB_OnRequest = ET.SubElement( MacroButton, 'MB_OnRequest')
-         MB_OnRequest.text = soundList(i) #path to sound file.mp3
-         MB_OffRequest = ET.SubElement( MacroButton, 'MB_OffRequest')
+i = 0
+while i < len(soundPath):
+   indexNumber = str(i+1)
+   MacroButton = ET.SubElement(MacroButtonConfiguration, 'MacroButton', index=indexNumber, type='0', color='3', key='85', ctrl='0', shift='0', alt='0', anyway='0', exclusive='0', trigger='0', xinput='0' )
+   MB_MIDI = ET.SubElement( MacroButton, 'MB_MIDI', b1='00', b2='00', b3='00', b4='00', b5='00', b6='00')
+   MB_TRIGGER = ET.SubElement( MacroButton, 'MB_TRIGGER', tchannel='0', tin='0.0', tout='0.0', tmsHold='100', tafterMute='0')
+   MB_XINPUT = ET.SubElement( MacroButton, 'MB_XINPUT', nctrl='0', nbutton='0')
+   MB_Name = ET.SubElement( MacroButton, 'MB_Name')
+   MB_Name.text = os.path.basename(soundPath[i]) #todo first N letters of File Name
+   MB_Subname = ET.SubElement( MacroButton, 'MB_Subname')
+   #MB_Subname.text = soundPath[i] # todo remaining N letters of File Name
+   MB_InitRequest = ET.SubElement( MacroButton, 'MB_InitRequest')
+   MB_InitRequest.text = 'Recorder.mode.PlayOnLoad=1'
+   MB_OnRequest = ET.SubElement( MacroButton, 'MB_OnRequest')
+   MB_OnRequest.text = 'Recorder.load=' + "\"" +soundPath[i] + "\""  #path to sound file.mp3
+   MB_OffRequest = ET.SubElement( MacroButton, 'MB_OffRequest')
+   i = i + 1
 
-tree.write('output.xml')
+buttonMap = ET.ElementTree(VBAudioVoicemeeterMacroButtonMap)
+ET.indent(buttonMap, space=" ", level=0)
+buttonMap.write(r'C:\Users\rblum\Google Drive\Game Files\soundBoard\Apex\output.xml', encoding='utf8', method='xml')
 
 
 #<?xml version="1.0" encoding="utf-8"?>
