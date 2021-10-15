f43c87be7d5f99ed9ab6985c69cca12355f9f16f
Robby <blumrj@gmail.com>
2021-10-14 20:14:32 -0400

    merged mouse utility to set soundboard window location

--- multiBoardGen.py
+++ multiBoardGen.py
@@ -4,6 +4,8 @@ import os
 import xml.etree.ElementTree as ET
 import sys
 from os.path import exists as file_exists
+from ctypes import windll, Structure, c_long, byref
+import time
 configparser = configparser.RawConfigParser()
 
 def getConfig():
@@ -19,6 +21,7 @@ def getConfig():
     global yPosition
     global setKeyboardShortCuts
     global rainbowButtons
+    global getMousePositionForWindow
     soundBoardPath = configparser.get('Config File', 'soundBoardPath')
     boardButtonColor = configparser.get('Config File', 'boardButtonColor')
     currentBoardButtonColor = configparser.get('Config File', 'currentBoardButtonColor')
@@ -28,6 +31,7 @@ def getConfig():
     yPosition = configparser.get('Config File', 'yPosition')
     setKeyboardShortCuts = configparser.get('Config File', 'setKeyboardShortCuts')
     rainbowButtons = configparser.get('Config File', 'rainbowButtons')
+    getMousePositionForWindow = configparser.get('Config File', 'getMousePositionForWindow')
     if rainbowButtons == "True":
         soundButtonColor = 0
     if checkExistingWindowConfig():
@@ -195,10 +199,62 @@ def getButton():
         print(ctrl)
         print(shift)
         print(alt)
+class POINT(Structure):
+    _fields_ = [("x", c_long), ("y", c_long)]
+
+def checkExistingWindowConfig():
+    configFilePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'windowConfig.txt'))
+    testFileList = []
+    testFileList = glob.glob(configFilePath)
+    if not testFileList:
+        return False
+    else:
+        return True
+
+def queryMousePosition():
+    global pt
+    pt = POINT()
+    windll.user32.GetCursorPos(byref(pt))
+    return { "x": pt.x, "y": pt.y}
+
+def writeConfig():
+    configFilePath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'windowConfig.txt'))
+    if checkExistingWindowConfig():
+        print("existing window config file found: updating")
+        configparser.read(configFilePath)
+        curxPosition = configparser.get('Window Config', 'xPosition')
+        curyPosition = configparser.get('Window Config', 'yPosition')
+        print( "changing current x position: " + str(curxPosition) + " to " + str(pt.x))
+        print( "changing current y position: " + str(curyPosition) + " to " + str(pt.y))
+        configparser.set('Window Config' , 'xPosition' , str(pt.x))
+        configparser.set('Window Config' , 'yPosition' , str(pt.y))
+    else:
+        print("existing window config file not found: creating")
+        configparser.add_section('Window Config')
+        configparser.set('Window Config' , 'xPosition' , str(pt.x))
+        configparser.set('Window Config' , 'yPosition' , str(pt.y))
+
+    f = open(configFilePath, "w")
+    print("writing Window Config to " + configFilePath)
+    configparser.write(f)
+
+def getMouse():
+    seconds = 0
+    print ("getting mouse position, place mouse where you want the top left of the button panel to be located")
+    while seconds < 5:
+        pos = queryMousePosition()
+
+        print ("writing location " + str(pos) + " to config in " + str(5-seconds) + " seconds")
+        seconds += 1
+        time.sleep(1)
+    print ("writing location " + str(pos) +" to config")
+    writeConfig()
 
 getConfig()
 checkExisting()
 setBoardList()
+if getMousePositionForWindow == "True":
+     getMouse()
 setWindow()
 printFoundDirs()
 
